c93071b536703d4c1d6ce8976acf8180f994f88f
TaeYai <TaeYai123@gmail.com>
2021-08-19 01:02:19 +0700

    new um position

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -904,7 +904,7 @@ class PlayState extends MusicBeatState
 						bop2.scale.set(1.2, 1.2);
 						bop2.frames = Paths.getSparrowAtlas('boppers/gar');
 						bop2.animation.addByPrefix('idle', 'gar bop1', 24, false);
-						bop3 = new FlxSprite(0, -20);
+						bop3 = new FlxSprite(0, 10);
 						bop3.scale.set(1.2, 1.2);
 						bop3.frames = Paths.getSparrowAtlas('boppers/ruvsarv');
 						bop3.animation.addByPrefix('idle', 'ruvsarv bop2', 24, false);
@@ -937,7 +937,7 @@ class PlayState extends MusicBeatState
 						bop2.scale.set(1.2, 1.2);
 						bop2.frames = Paths.getSparrowAtlas('boppers/gar');
 						bop2.animation.addByPrefix('idle', 'gar bop1', 24, false);
-						bop3 = new FlxSprite(0, -20);
+						bop3 = new FlxSprite(0, 10);
 						bop3.scale.set(1.2, 1.2);
 						bop3.frames = Paths.getSparrowAtlas('boppers/ruvsarv');
 						bop3.animation.addByPrefix('idle', 'ruvsarv bop2', 24, false);
